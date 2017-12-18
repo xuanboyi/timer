c008b9586c72796155b351e80418991da02d1ac2
玄博屹 <genhiroshi@xuanboyidemacbook-pro.local>
2017-12-19 07:23:39 +0800

    Initial Commit

---
+++ 定时器/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  定时器
+//
+//  Created by 玄博屹 on 2017/12/19.
+//  Copyright © 2017年 玄博屹. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
